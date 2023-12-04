e79b9842dac20bc917b20b2734191c9c8ef4eebf
raphael <rafi.colman@googlemail.com>
2023-12-04 08:38:31 +0000

    Day 4 part 1

--- src/Solutions/Day4.hs
+++ src/Solutions/Day4.hs
@@ -1,21 +1,50 @@
+{-# LANGUAGE RecordWildCards #-}
 module Solutions.Day4
-  ( aoc4
-  ) where
+    ( aoc4
+    ) where
 
 import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+                                      printSolutions, printTestSolutions)
+import           Control.Applicative (Alternative (some))
+import           Text.Parser.Char    (CharParsing (char))
+import           Text.Parser.Token   (integer, token)
+import           Text.Trifecta       (CharParsing (string), Parser, sepBy,
+                                      whiteSpace)
+import Combinatorics.Mastermind (Eval(white))
+import Data.List (union, intersect)
+import GHC.OldList (nub)
 
 aoc4 :: IO ()
 aoc4 = do
   printSolutions 4 $ MkAoCSolution parseInput part1
-  printSolutions 4 $ MkAoCSolution parseInput part2
+  --printSolutions 4 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+data Card
+  = MkCard
+      { _winners :: ![Integer]
+      , _entries :: ![Integer]
+      }
+  deriving (Eq, Show)
 
-part1 :: String -> String
-part1 = undefined
+parseInput = some $ token parseCard
+
+parseCard :: Parser Card
+parseCard = do
+  string "Card" >> whiteSpace >> integer >> char ':' >> whiteSpace
+  winners <- sepBy integer whiteSpace
+  string "|" >> whiteSpace
+  entries <- sepBy integer whiteSpace
+  pure $ MkCard winners entries
+
+part1 cards = sum $ map cardPoints cards
 
 part2 :: String -> String
-part2 = undefined
+part2 = id
+
+numWinners :: Card -> [Integer]
+numWinners MkCard {..} = _entries `intersect` _winners
+
+cardPoints :: Card -> Int
+cardPoints MkCard {..} = convertToPoints $ length $ _winners `intersect` _entries
+  where convertToPoints 0 = 0
+        convertToPoints x = 2^(x-1)
