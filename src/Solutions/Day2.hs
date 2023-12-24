fc34a021b2ccb8f48e52bea99f51653e10e676b7
raphael <rafi.colman@googlemail.com>
2023-12-24 17:36:36 +0000

    Day 2 parts 1 and 2

--- src/Solutions/Day2.hs
+++ src/Solutions/Day2.hs
@@ -1,21 +1,62 @@
+{-# LANGUAGE RecordWildCards #-}
 module Solutions.Day2
-  ( aoc2
-  ) where
+    ( aoc2
+    ) where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import           Common.AoCSolutions             (AoCSolution (MkAoCSolution),
+                                                  printSolutions,
+                                                  printTestSolutions)
+import           Control.Applicative.Combinators (some)
+import qualified Data.Map.Strict                 as M
+import           Debug.Trace
+import           Text.Parser.Char                (newline)
+import           Text.Trifecta                   (CharParsing (string), Parser,
+                                                  commaSep, integer, letter,
+                                                  semiSep)
+
+data Colour = Red | Green | Blue deriving (Bounded, Enum, Eq, Ord, Show)
+type Set = M.Map Colour Integer
+data Game
+  = MkGame
+      { _label :: !Integer
+      , _sets  :: ![Set]
+      }
+  deriving (Eq, Show)
 
 aoc2 :: IO ()
 aoc2 = do
   printSolutions 2 $ MkAoCSolution parseInput part1
   printSolutions 2 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+parseInput :: Parser [Game]
+parseInput = some $ parseGame <* newline
 
-part1 :: String -> String
-part1 = undefined
+parseGame :: Parser Game
+parseGame = do
+  label <- string "Game " *> integer <* string ": "
+  sets <- fmap (map M.fromList) $ semiSep $ commaSep parseCubes
+  pure $ MkGame label sets
 
-part2 :: String -> String
-part2 = undefined
+parseCubes :: Parser (Colour, Integer)
+parseCubes = do
+  num <- integer
+  c <- some letter
+  case c of
+    "red"   -> return (Red, num)
+    "green" -> return (Green, num)
+    "blue"  -> return (Blue, num)
+    _       -> fail $ "Invalid colour: " ++ c
+
+part1 :: [Game] -> Integer
+part1 = sum . map _label . filter (possibleGame testSet)
+  where testSet = M.fromList [(Red, 12), (Green, 13), (Blue, 14)]
+
+part2 :: [Game] -> Integer
+part2 = sum . map (M.foldr (*) 1 . fewestCubes)
+
+possibleGame :: Set -> Game -> Bool
+possibleGame set MkGame{..} = all (possible set) _sets
+  where possible test cubes = all ((>=0) . snd) $ M.toList $ M.unionWith (-) test cubes
+
+fewestCubes :: Game -> Set
+fewestCubes MkGame{..} = M.unionsWith max _sets
