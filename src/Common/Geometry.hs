322c43abe5b385d063d601293e430ba9e838a825
raphael <rafi.colman@googlemail.com>
2023-12-28 14:26:41 +0000

    Day 3 part 1 - only works on test input

--- src/Common/Geometry.hs
+++ src/Common/Geometry.hs
@@ -1,15 +1,16 @@
 module Common.Geometry where
 
-import           Control.Lens    ((^.))
+import           Control.Lens    (Field1 (_1), Field2 (_2), (^.))
 import           Data.Foldable   (maximumBy, minimumBy)
+import           Data.List
 import           Data.List.Split (chunksOf)
 import qualified Data.Map        as M hiding (mapMaybe)
 import           Data.Maybe      (mapMaybe)
 import           Data.Sequence   (Seq)
 import qualified Data.Sequence   as Seq
 import qualified Data.Set        as S
+import           Linear          (unit)
 import           Linear.V2       (R1 (_x), R2 (_y), V2 (..))
-import Linear (unit)
 
 type Point = V2 Int
 
@@ -65,7 +66,7 @@ renderVectorSet points =
    in renderVectorMap asMap
 
 renderVectorList :: [Point] -> String
-renderVectorList = renderVectorSet . S.fromList 
+renderVectorList = renderVectorSet . S.fromList
 
 allOrthogonalDirections :: [V2 Int]
 allOrthogonalDirections = [unit _x, -unit _x, unit _y, -unit _y]
@@ -78,3 +79,16 @@ gridOrthogonalNeighbours grid point = M.restrictKeys grid $ allOrthogonalNeighbo
 
 manhattanDistance :: Point -> Point -> Int
 manhattanDistance (V2 x1 y1) (V2 x2 y2) = abs (x1 - x2) + abs (y1 - y2)
+
+toOrderedList :: Grid a -> [(Point, a)]
+toOrderedList = sortBy sortFun . M.toList
+  where sortFun :: (Point, a) -> (Point, a) -> Ordering
+        sortFun p1 p2 = let onY = compare (p1 ^. _1 . _y) (p2 ^. _1 . _y)
+                        in case onY of
+                                EQ -> compare (p1 ^. _1 . _x) (p2 ^. _1 . _x)
+                                _  -> onY
+
+toRows :: Grid a -> [[(Point, a)]]
+toRows grid = chunksOf ((maxX ^. _x) + 1) ordered
+  where ordered = toOrderedList grid
+        maxX = maximumBy (\a b -> compare (a ^. _x) (b ^. _x)) $ M.keys grid
--- src/Solutions/Day3.hs
+++ src/Solutions/Day3.hs
@@ -1,21 +1,61 @@
 module Solutions.Day3
-  ( aoc3
-  ) where
+    ( aoc3
+    ) where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import           Common.AoCSolutions             (AoCSolution (MkAoCSolution),
+                                                  printSolutions,
+                                                  printTestSolutions)
+import           Common.FunctorUtils             (fmap2)
+import           Common.Geometry                 (Grid, Point,
+                                                  enumerateMultilineStringToVectorMap,
+                                                  gridNeighbours, neighbours,
+                                                  toOrderedList, toRows)
+import           Control.Applicative.Combinators (some)
+import           Control.Lens                    ()
+import           Data.Char                       (isDigit)
+import           Data.Foldable                   (maximumBy, minimumBy)
+import           Data.List                       (groupBy, sortBy)
+import qualified Data.Map.Strict                 as M
+import qualified Data.Set                        as S
+import           Debug.Trace                     (traceShow)
+import           Linear                          (V2 (..))
+import           Linear.V2                       (_x, _y)
+import           Safe                            (headMay)
+import           Text.Parser.Char                (CharParsing (anyChar))
+import           Text.Trifecta                   (Parser)
 
 aoc3 :: IO ()
 aoc3 = do
   printSolutions 3 $ MkAoCSolution parseInput part1
-  printSolutions 3 $ MkAoCSolution parseInput part2
+  --printSolutions 3 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+parseInput :: Parser (Grid Char)
+parseInput = do
+  allChars <- some anyChar
+  pure $ enumerateMultilineStringToVectorMap allChars
 
-part1 :: String -> String
-part1 = undefined
+part1 = solve
 
-part2 :: String -> String
+
 part2 = undefined
+
+
+solve grid = sum numbersNextToSymbols
+  where symbols = M.filter (\c -> not (isDigit c || c == '.')) grid
+        nums = fetchNumbers grid
+        numbersNextToSymbols = S.fromList $ convertToInts $ filter (isNextToSymbol grid) nums
+        allNumbers = S.fromList $ convertToInts nums
+
+convertToInts :: [[(Point, Char)]] -> [Int]
+convertToInts = map read . fmap2 snd
+
+fetchNumbers :: M.Map Point Char -> [[(Point, Char)]]
+fetchNumbers grid = concatMap justNumbers asRows
+  where asRows = toRows grid
+        justNumbers row = filter (maybe False (isDigit . snd) . headMay) $
+                          groupBy (\(_, c1) (_, c2) -> all isDigit [c1, c2] || (c1 == c2)) row
+
+isNextToSymbol :: Grid Char -> [(Point, Char)] -> Bool
+isNextToSymbol grid points = not (null adjacentSymbols)
+    where neighbourBlock = S.unions $ map (neighbours . fst) points
+          adjacentSymbols = M.filter (\c -> not (isDigit c || c == '.')) $ M.restrictKeys grid neighbourBlock
